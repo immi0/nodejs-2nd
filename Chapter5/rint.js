5e549065d2dac00eae297cee1a798e120fb84c3a
immi0 <saint.sohn@gmail.com>
2018-11-15 10:44:38 +0900

    All Examples

---
+++ Chapter5/rint.js
@@ -0,0 +1,9 @@
+﻿// Event Emitter 객체를 생성합니다.
+//exports.timer = new process.EventEmitter();
+var events = require('events');
+exports.timer = new events();
+
+// 이벤트를 강제로 발생시킵니다.
+setInterval(function () {
+    exports.timer.emit('tick');
+}, 1000);
---
+++ Chapter6/4th/request.cookie.js
@@ -0,0 +1,39 @@
+﻿// 모듈을 추출합니다.
+var http = require('http');
+
+// 모듈을 사용합니다.
+http.createServer(function (request, response) {
+    // 쿠키를 추출하고 분해합니다.
+    if(request.headers.cookie) {
+    	var cookie = request.headers.cookie;
+    	    cookie = cookie.split(';').map(function (element) {
+            var element = element.split('=');
+            return {
+                key: element[0],
+                value: element[1]
+            };
+    	});
+
+        // Request 헤더의 쿠키 정보만 출력
+        var content = '<h1>Cookies</h1>';
+        content += '<h2>'+JSON.stringify(cookie)+'</h2>';
+        // Request 헤더 전체 
+        content += '<h1>Request headers</h1>';
+        content += '<h2>'+JSON.stringify(request.headers)+'</h2>';
+ 
+        response.end(content);
+    // Request 헤더에 쿠키 정보가 없으면 쿠키 생성
+    }
+    else {		
+    // 쿠키를 생성합니다.
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+            'Set-Cookie': ['name=RintIanTta', 'region=Seoul']
+        });
+
+        // GET 요청
+        response.end('<h1>' + JSON.stringify(cookie) + '</h1>');
+    }
+}).listen(52273, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
